bb2c28358324db1581de8c02e98c2787d0d6a89c
manojmukunthavasan <129317979+manojmukunthavasan@users.noreply.github.com>
2023-04-10 16:36:54 +0530

    system

---
+++ system/Computer.java
@@ -0,0 +1,14 @@
+package org.system;
+
+public class Computer{
+	
+	public void computerModel() {
+		System.out.println("Legion 5");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ system/Desktop.java
@@ -0,0 +1,16 @@
+package org.system;
+
+public class Desktop extends Computer {
+	
+	public void desktopSize() {
+		System.out.println("17.5");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Desktop ds = new Desktop();
+    	 ds.computerModel();
+    	 ds.desktopSize();
+	}
+
+}
