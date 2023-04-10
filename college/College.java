d4aad00e81f9039db387711b41b5ca90d7b17d35
manojmukunthavasan <129317979+manojmukunthavasan@users.noreply.github.com>
2023-04-10 17:02:51 +0530

    college

---
+++ college/College.java
@@ -0,0 +1,18 @@
+package org.college;
+
+public class College {
+	
+	public void collegeName() {
+		System.out.println("The college name is: Crescent University");
+	}
+	public void collegeCode() {
+		System.out.println("code is 156585");
+		
+	}
+	public void collegeRank() {
+		System.out.println("rank is 125");
+	}
+
+	
+
+}
